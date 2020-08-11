cdcb8f7868573ad964beab544bb77b12c27e8b3e
Allan <vredes@live.com>
2020-08-10 23:44:00 -0400

    nuevo diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
-import { environment } from '../environments/environment'
+import { environment } from '../environments/environment';
+
+//Componentes
+import { ChatComponent } from './component/chat/chat.component'
+
+//Services
+import { ChatService } from "../app/services/chat.service"
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'my-app-name'), // imports firebase/app needed for everything
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
